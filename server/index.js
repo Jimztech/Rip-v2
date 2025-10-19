154f4782de7469ece7cda606ba8c626ecf36f4f2
jimztech <ibezimjames20@gmail.com>
2025-10-19 03:58:19 +0200

    updated PORT

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import { GoogleGenAI } from '@google/genai';
 dotenv.config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
